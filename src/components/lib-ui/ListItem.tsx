90cdc8841998257ee492a2ff1620aa0275bf528d
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2025-09-20 15:27:56 -0400

    fix: fix for mobile screens

--- src/components/lib-ui/ListItem.tsx
+++ src/components/lib-ui/ListItem.tsx
@@ -1,45 +1,47 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { MoreHorizontal } from "lucide-react"
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { MoreHorizontal } from 'lucide-react'
 
 type CardProps = React.ComponentProps<typeof Card> & {
-  title?: string,
-  footer?: string
-  description?: string
+    title?: string
+    footer?: string
+    description?: string
 }
 
 export function ListItem({
-  className, title, children, description, ...props
+    className,
+    title,
+    children,
+    description,
+    ...props
 }: CardProps) {
-  return (
-    <Card className={cn("group w-full", className)} {...props}>
-      <CardHeader className="flex flex-row items-center justify-between">
-        <div className="flex w-full flex-col items-start">
-          <CardTitle>{title}</CardTitle>
-          <small>{description}</small>
-        </div>
-        {children ? (
-          <div className="flex items-center gap-2">
-            <div className="flex items-center gap-2 origin-right scale-0 opacity-0 pointer-events-none transition-all duration-200 ease-in group-hover:scale-100 group-hover:opacity-100 group-hover:pointer-events-auto group-focus-within:scale-100 group-focus-within:opacity-100 group-focus-within:pointer-events-auto">
-              {children}
-            </div>
-            <Button
-              type="button"
-              variant="ghost"
-              size="icon"
-              aria-label="Toggle actions"
-              className="h-9 w-9 rounded-full transition duration-200 ease-in"
-            >
-              <MoreHorizontal className="h-5 w-5" />
-            </Button>
-          </div>
-        ) : null}
-      </CardHeader>
-    </Card>
-  )
+    return (
+        <Card className={cn('group w-full', className)} {...props}>
+            <CardHeader className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+                <div className="flex w-full flex-col items-start">
+                    <CardTitle>{title}</CardTitle>
+                    <small>{description}</small>
+                </div>
+                {children ? (
+                    <>
+                        <div className="items-center gap-2 flex">
+                            <div className="flex items-center gap-2 origin-right scale-0 opacity-0 pointer-events-none transition-all duration-200 ease-in group-hover:scale-100 group-hover:opacity-100 group-hover:pointer-events-auto group-focus-within:scale-100 group-focus-within:opacity-100 group-focus-within:pointer-events-auto">
+                                {children}
+                            </div>
+                            <Button
+                                type="button"
+                                variant="ghost"
+                                size="icon"
+                                aria-label="Toggle actions"
+                                className="h-9 w-9 rounded-full transition duration-200 ease-in"
+                            >
+                                <MoreHorizontal className="h-5 w-5" />
+                            </Button>
+                        </div>
+                    </>
+                ) : null}
+            </CardHeader>
+        </Card>
+    )
 }
