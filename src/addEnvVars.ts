11f1f732168d8fc2e6fbd25224c5b6ff424fc019
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-08-26 19:04:38 -0400

    change ui colors

---
+++ src/addEnvVars.ts
@@ -0,0 +1,339 @@
+export const addEnvVars = () =>{
+const root = document.documentElement;
+
+console.log(
+  'VITE_LIGHT_BACKGROUND:',
+  import.meta.env.VITE_LIGHT_BACKGROUND
+)
+
+  root.style.setProperty(
+  '--env-light-background',
+  import.meta.env.VITE_LIGHT_BACKGROUND
+)
+
+root.style.setProperty(
+  '--env-light-foreground',
+  import.meta.env.VITE_LIGHT_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-card',
+  import.meta.env.VITE_LIGHT_CARD
+)
+
+root.style.setProperty(
+  '--env-light-card-foreground',
+  import.meta.env.VITE_LIGHT_CARD_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-popover',
+  import.meta.env.VITE_LIGHT_POPOVER
+)
+
+root.style.setProperty(
+  '--env-light-popover-foreground',
+  import.meta.env.VITE_LIGHT_POPOVER_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-primary',
+  import.meta.env.VITE_LIGHT_PRIMARY
+)
+
+root.style.setProperty(
+  '--env-light-primary-foreground',
+  import.meta.env.VITE_LIGHT_PRIMARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-secondary',
+  import.meta.env.VITE_LIGHT_SECONDARY
+)
+
+root.style.setProperty(
+  '--env-light-secondary-foreground',
+  import.meta.env.VITE_LIGHT_SECONDARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-muted',
+  import.meta.env.VITE_LIGHT_MUTED
+)
+
+root.style.setProperty(
+  '--env-light-muted-foreground',
+  import.meta.env.VITE_LIGHT_MUTED_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-accent',
+  import.meta.env.VITE_LIGHT_ACCENT
+)
+
+root.style.setProperty(
+  '--env-light-accent-foreground',
+  import.meta.env.VITE_LIGHT_ACCENT_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-destructive',
+  import.meta.env.VITE_LIGHT_DESTRUCTIVE
+)
+
+root.style.setProperty(
+  '--env-light-destructive-foreground',
+  import.meta.env.VITE_LIGHT_DESTRUCTIVE_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-border',
+  import.meta.env.VITE_LIGHT_BORDER
+)
+
+root.style.setProperty(
+  '--env-light-input',
+  import.meta.env.VITE_LIGHT_INPUT
+)
+
+root.style.setProperty(
+  '--env-light-ring',
+  import.meta.env.VITE_LIGHT_RING
+)
+
+root.style.setProperty(
+  '--env-light-chart-1',
+  import.meta.env.VITE_LIGHT_CHART_1
+)
+
+root.style.setProperty(
+  '--env-light-chart-2',
+  import.meta.env.VITE_LIGHT_CHART_2
+)
+
+root.style.setProperty(
+  '--env-light-chart-3',
+  import.meta.env.VITE_LIGHT_CHART_3
+)
+
+root.style.setProperty(
+  '--env-light-chart-4',
+  import.meta.env.VITE_LIGHT_CHART_4
+)
+
+root.style.setProperty(
+  '--env-light-chart-5',
+  import.meta.env.VITE_LIGHT_CHART_5
+)
+
+root.style.setProperty(
+  '--env-light-radius',
+  import.meta.env.VITE_LIGHT_RADIUS
+)
+
+root.style.setProperty(
+  '--env-light-sidebar',
+  import.meta.env.VITE_LIGHT_SIDEBAR
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-foreground',
+  import.meta.env.VITE_LIGHT_SIDEBAR_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-primary',
+  import.meta.env.VITE_LIGHT_SIDEBAR_PRIMARY
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-primary-foreground',
+  import.meta.env.VITE_LIGHT_SIDEBAR_PRIMARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-accent',
+  import.meta.env.VITE_LIGHT_SIDEBAR_ACCENT
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-accent-foreground',
+  import.meta.env.VITE_LIGHT_SIDEBAR_ACCENT_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-border',
+  import.meta.env.VITE_LIGHT_SIDEBAR_BORDER
+)
+
+root.style.setProperty(
+  '--env-light-sidebar-ring',
+  import.meta.env.VITE_LIGHT_SIDEBAR_RING
+)
+
+root.style.setProperty(
+  '--env-dark-background',
+  import.meta.env.VITE_DARK_BACKGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-foreground',
+  import.meta.env.VITE_DARK_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-card',
+  import.meta.env.VITE_DARK_CARD
+)
+
+root.style.setProperty(
+  '--env-dark-card-foreground',
+  import.meta.env.VITE_DARK_CARD_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-popover',
+  import.meta.env.VITE_DARK_POPOVER
+)
+
+root.style.setProperty(
+  '--env-dark-popover-foreground',
+  import.meta.env.VITE_DARK_POPOVER_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-primary',
+  import.meta.env.VITE_DARK_PRIMARY
+)
+
+root.style.setProperty(
+  '--env-dark-primary-foreground',
+  import.meta.env.VITE_DARK_PRIMARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-secondary',
+  import.meta.env.VITE_DARK_SECONDARY
+)
+
+root.style.setProperty(
+  '--env-dark-secondary-foreground',
+  import.meta.env.VITE_DARK_SECONDARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-muted',
+  import.meta.env.VITE_DARK_MUTED
+)
+
+root.style.setProperty(
+  '--env-dark-muted-foreground',
+  import.meta.env.VITE_DARK_MUTED_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-accent',
+  import.meta.env.VITE_DARK_ACCENT
+)
+
+root.style.setProperty(
+  '--env-dark-accent-foreground',
+  import.meta.env.VITE_DARK_ACCENT_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-destructive',
+  import.meta.env.VITE_DARK_DESTRUCTIVE
+)
+
+root.style.setProperty(
+  '--env-dark-destructive-foreground',
+  import.meta.env.VITE_DARK_DESTRUCTIVE_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-border',
+  import.meta.env.VITE_DARK_BORDER
+)
+
+root.style.setProperty(
+  '--env-dark-input',
+  import.meta.env.VITE_DARK_INPUT
+)
+
+root.style.setProperty(
+  '--env-dark-ring',
+  import.meta.env.VITE_DARK_RING
+)
+
+root.style.setProperty(
+  '--env-dark-chart-1',
+  import.meta.env.VITE_DARK_CHART_1
+)
+
+root.style.setProperty(
+  '--env-dark-chart-2',
+  import.meta.env.VITE_DARK_CHART_2
+)
+
+root.style.setProperty(
+  '--env-dark-chart-3',
+  import.meta.env.VITE_DARK_CHART_3
+)
+
+root.style.setProperty(
+  '--env-dark-chart-4',
+  import.meta.env.VITE_DARK_CHART_4
+)
+
+root.style.setProperty(
+  '--env-dark-chart-5',
+  import.meta.env.VITE_DARK_CHART_5
+)
+
+root.style.setProperty(
+  '--env-dark-radius',
+  import.meta.env.VITE_DARK_RADIUS
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar',
+  import.meta.env.VITE_DARK_SIDEBAR
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-foreground',
+  import.meta.env.VITE_DARK_SIDEBAR_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-primary',
+  import.meta.env.VITE_DARK_SIDEBAR_PRIMARY
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-primary-foreground',
+  import.meta.env.VITE_DARK_SIDEBAR_PRIMARY_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-accent',
+  import.meta.env.VITE_DARK_SIDEBAR_ACCENT
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-accent-foreground',
+  import.meta.env.VITE_DARK_SIDEBAR_ACCENT_FOREGROUND
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-border',
+  import.meta.env.VITE_DARK_SIDEBAR_BORDER
+)
+
+root.style.setProperty(
+  '--env-dark-sidebar-ring',
+  import.meta.env.VITE_DARK_SIDEBAR_RING
+)
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { UiSettingsProvider } from './components/lib-ui/UiSettingsContext'
+import { addEnvVars } from './addEnvVars.ts'
+
+addEnvVars();
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
